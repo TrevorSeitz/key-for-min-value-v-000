125df2cfde6a3dde662aa2630706b9ce4746200e
Trevor Seitz <trevinla@gmail.com>
2018-03-14 15:52:36 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   low_price = nil
   low_item = nil
   name_hash.each do |item, price|
-    if price < low_price || lowest_price == nil
+    if price < low_price || low_price == nil
       low_price = price
       low_item = item
     end
