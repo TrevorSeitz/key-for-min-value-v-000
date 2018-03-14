b559aa99385ff8cba8fc20c8c17c5c3971e2e3f6
Trevor Seitz <trevinla@gmail.com>
2018-03-14 15:51:14 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,18 +1,18 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 # require "pry"
-# def key_for_min_value(name_hash)
+def key_for_min_value(name_hash)
 #   low_price = Float::INFINITY
-#   low_price = nil
-#   low_item = nil
-#   name_hash.each do |item, price|
-#     if price < low_price || lowest_price
-#       low_price = price
-#       low_item = item
-#     end
-# end
-# return low_item
-# end
+  low_price = nil
+  low_item = nil
+  name_hash.each do |item, price|
+    if price < low_price || lowest_price
+      low_price = price
+      low_item = item
+    end
+end
+return low_item
+end
 
 # def key_for_min_value(hash)
 #    lowest_item = nil
