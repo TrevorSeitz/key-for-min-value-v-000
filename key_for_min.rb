b6e5eaf45d6c80db2df5e6f8155e679a9e8e280d
Trevor Seitz <trevinla@gmail.com>
2018-03-14 15:45:35 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   # end
   # low_price = 10000
   low_price = Float::INFINITY
-  low_item = NIL
+  low_item = nil
   name_hash.each do |item, price|
     if price < low_price
       price = low_price
